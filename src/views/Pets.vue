a59351ba9a230d84acfafb449ffa8b4de09862c7
Obinna <oisiwekpeni@gmail.com>
2019-11-09 03:08:28 +0100

    First commit

---
+++ src/views/Pets.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Name: {{ animal.name }}</h1>
+    <p>Breed: {{ animal.breed }} ({{$route.params.species}})</p>
+    <p>{{ animal.age }} years old</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
+
+<style>
+
+</style>
